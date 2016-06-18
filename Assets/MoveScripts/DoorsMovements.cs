06f6a5bcbe5a40bf60bd7c394273e2328cebdcae
giomm2 <giomm269@gmail.com>
2016-06-18 10:13:28 -0600

    Scripts de movimiento objetos.

---
+++ Assets/MoveScripts/DoorsMovements.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DoorsMovements : MonoBehaviour
+{
+    private float y;
+    private float x;
+    private float z;
+    public AudioClip audioOpen;
+    public AudioClip audioClose;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public int timer;
+
+    //Its a int becuase:
+    //0:change position
+    //1:Back position
+    //3:Dont move
+    private int flag=3;
+    private bool flag2 = true;
+    private bool flag3 = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        gameObject.AddComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((int)Time.time - timer == 2)
+        {
+            source.Stop();
+
+        }
+
+        if ((int)Time.time == timer)
+        {
+            flag = 0;
+        }
+
+        if ((int)Time.time > timer)
+        {
+            flag = 1;
+        }
+
+        if (flag == 0 || flag == 1)
+        {
+
+            DoorsMovement();
+        }
+
+
+
+    }
+
+    public void DoorsMovement()
+    {
+
+
+        if (flag2)
+        {
+
+            transform.localPosition = new Vector3(-206.2145f, 0, -238.7241f);
+            transform.localRotation = Quaternion.Euler(0, 19.4667f, 0);
+            gameObject.AddComponent<AudioSource>();
+            source.clip = audioOpen;
+            source.playOnAwake = false;
+            source.PlayOneShot(audioOpen);
+
+            flag2 = false;
+
+        }
+
+        if (flag == 1 && (int)Time.time - 5 == timer && flag3 == true)
+        {
+
+            transform.localPosition = new Vector3(0, 0, 0);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+            gameObject.AddComponent<AudioSource>();
+            source.clip = audioClose;
+            source.playOnAwake = false;
+            source.PlayOneShot(audioClose);
+            flag = 3;
+
+            flag3 = false;
+
+        }
+
+
+
+
+    }
+
+
+
+}
+
--- Assets/MoveScripts/MoveObjects.cs
+++ Assets/MoveScripts/MoveObjects.cs
@@ -12,70 +12,62 @@ public class MoveObjects : MonoBehaviour
     float z;
     public AudioClip audio;
     private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public int rdn;
+   private bool flag;
+    void Start()
+    {
+
+       
+    }
+
 
    
-    // Update is called once per frame
-
-
-    void Start() {
-
-
-    }
     void Update()
 
-
-    { 
-
-        PictureM();
- 
-    }
-
-
-    public void PictureM()
+        
     {
-            y += -1f * (int)Time.time;
-            x += 2 * (int)Time.time;
 
-            if (y > -5)
-            {
-
-                transform.localPosition = new Vector3(0, y, 0);
-                transform.localRotation = Quaternion.Euler(x, 0, 0);
-
-
-                if ((y - 1) == -5)
-                {
-                    gameObject.AddComponent<AudioSource>();
-                    source.clip = audio;
-                    source.playOnAwake = false;
-                    source.PlayOneShot(audio);
-                }
-            
-        }
-    }
-
-
-    public void DoorsM()
-    {
-      //  transform.localPosition = new Vector3(5, 0, 0);
-        y += 0.01f * (int)Time.time;
-        x += -0.1f * (int)Time.time;
-        z += -1f * (int)Time.time;
-
-        if (x < -30)
+    if((int)Time.time == rdn)
         {
-            transform.localPosition = new Vector3(-206.2145f, 0, -238.7241f);
-            transform.localRotation = Quaternion.Euler(0, 19.4667f, 0);
-
-          
+            flag = true;
         }
 
-        if (x > -29) {
 
-            transform.localPosition = new Vector3(0, 0, 0);
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        if (flag)
+        {
+
+            PictureMovement();
         }
-        
-        
+
     }
+
+
+   
+
+    public void PictureMovement()
+    {
+       
+        y += -1f * ((int)Time.time-rdn);
+        x += 2 * ((int)Time.time - rdn);
+        
+        if (y > -5)
+        {
+
+            transform.localPosition = new Vector3(0, y, 0);
+            transform.localRotation = Quaternion.Euler(x, 0, 0);
+
+
+            if ((y - 1) == -5)
+            {
+                gameObject.AddComponent<AudioSource>();
+                source.clip = audio;
+                source.playOnAwake = false;
+                source.PlayOneShot(audio);
+            }
+
+        }
+    }
+
+
+   
 }
